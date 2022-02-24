50eafe597031aaf77294b99e86a8e146f3facf0b
Artemsav <31589472+Artemsav@users.noreply.github.com>
2022-02-24 23:13:12 +0300

    fix formatting

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,26 +1,45 @@
+import argparse
+import json
+import os
+import pathlib
+from pathlib import Path
+from urllib.parse import urljoin, urlsplit
+
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin
-from urllib.parse import urlsplit
-from lib_request import download_txt, download_image, parse_book_page, check_for_redirect
-import json
 from pathvalidate import sanitize_filename
-import argparse
-from pathlib import Path
-import pathlib
-import os
+
+from lib_request import (check_for_redirect, download_image, download_txt,
+                         parse_book_page)
 
 
 def parse_user_input():
     """Функция для парсинга пользовательского ввода
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--start_page', help='Start page of book to be parsed', type=int, default=1)
-    parser.add_argument('-e', '--end_page', help='End page of book to be parsed', type=int, default=2)
-    parser.add_argument('-df', '--dest_folder', help='Folder for parsing book and images', action='store')
-    parser.add_argument('-si', '--skip_imgs', action='store_true', help='Skip images parsing')
-    parser.add_argument('-st', '--skip_txt', action='store_true', help='Skip .txt parsing')
-    parser.add_argument('-jp', '--json_path', help='Json path')
+    parser.add_argument('-s', '--start_page',
+                        help='Start page of book to be parsed',
+                        type=int, default=1,
+                        )
+    parser.add_argument('-e', '--end_page',
+                        help='End page of book to be parsed',
+                        type=int, default=2,
+                        )
+    parser.add_argument('-df', '--dest_folder',
+                        help='Folder for parsing book and images',
+                        action='store',
+                        )
+    parser.add_argument('-si', '--skip_imgs',
+                        action='store_true',
+                        help='Skip images parsing',
+                        )
+    parser.add_argument('-st', '--skip_txt',
+                        action='store_true',
+                        help='Skip .txt parsing',
+                        )
+    parser.add_argument('-jp', '--json_path',
+                        help='Json path',
+                        )
     args = parser.parse_args()
     return args
 
@@ -58,22 +77,33 @@ if __name__ == '__main__':
                     split_book_url = book_link.split('b')
                     _, book_id = split_book_url
                     title_text = book_data.get('title')
-                    filename = '{book_id}. {title_text}'.format(book_id=book_id, title_text=title_text)
+                    filename = '{book_id}. {title_text}'.format(book_id=book_id,
+                                                                title_text=title_text,
+                                                                )
                     if user_input.skip_txt:
                         pass
                     else:
-                        download_txt(url=txt_link, filename=filename, folder=books_path)
+                        download_txt(url=txt_link, filename=filename,
+                                     folder=books_path,
+                                     )
                     if image_link:
                         image_link = book_data.get('picture_link')
                         image_name = urlsplit(image_link)[2].split('/')[-1]
                         if user_input.skip_imgs:
                             pass
                         else:
-                            download_image(url=urljoin(main_url, image_link), filename=image_name, folder=images_path)
+                            download_image(url=urljoin(main_url, image_link),
+                                           filename=image_name,
+                                           folder=images_path,
+                                           )
                     book_inf = {'title': title_text,
                                 'author': book_data.get('author'),
-                                'img_src': '{images_path}{image_name}'.format(images_path=images_path , image_name=image_name),
-                                'book_path': '{books_path}{filename}.txt'.format(books_path=books_path, filename=sanitize_filename(filename)),
+                                'img_src': '{images_path}{image_name}'.format(images_path=images_path,
+                                                                              image_name=image_name,
+                                                                              ),
+                                'book_path': '{books_path}{filename}.txt'.format(books_path=books_path,
+                                                                                 filename=sanitize_filename(filename),
+                                                                                 ),
                                 'comments': book_data.get('comments'),
                                 'genres': book_data.get('genres'),
                                 }
@@ -85,7 +115,9 @@ if __name__ == '__main__':
         Path(path).mkdir(exist_ok=True)
         named_path = '{path}/{filename}'.format(path=path, filename=filename)
     elif user_input.dest_folder:
-        named_path = '{path}/{filename}'.format(path=user_input.dest_folder, filename=filename)
+        named_path = '{path}/{filename}'.format(path=user_input.dest_folder,
+                                                filename=filename,
+                                                )
     else:
         named_path = filename
     with open(named_path, 'w') as book_json:
