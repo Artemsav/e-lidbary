f78a9a4784059a20fc08b2c7e6bea34732e74b5a
artemsav <studentbedn@gmail.com>
2022-03-14 17:00:47 +0300

    fix distribution of data between columns in template

--- render_website.py
+++ render_website.py
@@ -1,5 +1,5 @@
 import json
-from more_itertools import chunked
+from more_itertools import distribute
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
 
@@ -7,12 +7,13 @@ from livereload import Server
 def rebuild():
     with open('result.json', 'r') as file:
         data = json.load(file)
+    distributed_data = [list(books) for books in distribute(2, data)]
     env = Environment(loader=FileSystemLoader('.'),
                       autoescape=select_autoescape(['html', 'xml'])
                       )
     template = env.get_template('template.html')
     rendered_page = template.render(
-      data=data,
+      data=distributed_data,
     )
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
