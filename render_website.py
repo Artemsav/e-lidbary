7e680758d5ec7613c778069a5025915bff803a0d
Artemsav <31589472+Artemsav@users.noreply.github.com>
2022-04-03 17:11:41 +0300

    fix naming

--- render_website.py
+++ render_website.py
@@ -8,14 +8,14 @@ from pathlib import Path
 def rebuild():
     with open('result.json', 'r') as file:
         books = json.load(file)
-    books_in_row = 20
-    chunked_books = list(chunked(books, books_in_row))
+    books_on_page = 20
+    chunked_books = list(chunked(books, books_on_page))
     env = Environment(loader=FileSystemLoader('.'),
                       autoescape=select_autoescape(['html', 'xml'])
                       )
     for i, books in enumerate(chunked_books, 1):
         template = env.get_template('template.html')
-        books_columns = list(chunked(books, books_in_row//2))
+        books_columns = list(chunked(books, books_on_page//2))
         number_of_page = len(chunked_books)
         rendered_page = template.render(books_columns=books_columns,
                                         number_of_page=number_of_page,
